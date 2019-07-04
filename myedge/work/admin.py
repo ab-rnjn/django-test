21d1ab59eee3683a944b454cbf2e4c35b801b800
suruchi singh <14.suruchi@gmail.com>
2019-07-04 23:00:53 +0530

    change in models, admin from suruchi
    Signed-off-by: suruchi singh <14.suruchi@gmail.com>

--- myedge/work/admin.py
+++ myedge/work/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Campaign,Upcoming_Events,Gallery
+admin.site.register(Campaign)
+admin.site.register(Upcoming_Events)
+admin.site.register(Gallery)
--- myedge/work/models.py
+++ myedge/work/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Campaign(models.Model):
+    campaign_image=models.ImageField()
+    campaign_title=models.CharField(max_length=100)
+    campaign_text=models.TextField()
+
+class Upcoming_Events(models.Model):
+    event_images=models.ImageField()
+    event_title=models.CharField(max_length=100)
+    event_date=models.DateField()
+    event_venue=models.CharField(max_length=20)
+
+class Gallery(models.Model):
+    gallery_image=models.ImageField()
